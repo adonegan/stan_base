de793a95f29c07e70f28e43965f819e0c3448e1d
adonegan <48014273+adonegan@users.noreply.github.com>
2019-11-23 17:52:13 +0000

    added mongo config details

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import os 
+import env
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import env
-MONGO_URI = os.getenv('MONGO_URI', "Env value not loaded")
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'stan_base'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env value not loaded")
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_films')
 def get_films():
-    return render_template("films.html", films=monggo.db.film.find())
+    return render_template("films.html", films=mongo.db.films.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
