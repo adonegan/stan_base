444ab74df20265219b879276d86f07e9d46ed946
adonegan <48014273+adonegan@users.noreply.github.com>
2019-11-23 17:29:44 +0000

    added more imports and first function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import env
@@ -10,8 +10,9 @@ app = Flask(__name__)
 mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World... again'
+@app.route('/get_films')
+def get_films():
+    return render_template("films.html", films=monggo.db.film.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
