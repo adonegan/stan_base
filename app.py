e1bf080862c10c90fbd648818d96e2fc56a4659d
adonegan <48014273+adonegan@users.noreply.github.com>
2019-12-12 16:07:12 +0000

    readded home.html after testing

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")      
+    return render_template("home.html")     
 
 
 @app.route('/get_films')
